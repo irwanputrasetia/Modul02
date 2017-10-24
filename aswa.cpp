ebf93b02ba8c9b77e5a354d76e24cfafbb419be5
irwanputrasetia <32324811+irwanputrasetia@users.noreply.github.com>
2017-10-24 20:46:35 +0700

    Membuat baru praktek0204

---
+++ aswa.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int LRumah=42;
+	cout<<"Luas Rumah			: "<<LRumah<<" m2"<<endl;
+	double LBP=1.87596;
+	cout<<"Luas 1 box parket	: "<<LBP<<" m2"<<endl;
+	double JBP=LRumah/LBP;
+	cout<<"Jumlah box parket	: "<<JBP<<endl;
+	double BP=JBP*500;
+	cout<<"Biaya Parket (Rp)	: "<<BP<<endl;
+	int Bpas=LRumah*20;
+	cout<<"Biaya Pasang (Rp)	: "<<Bpas<<endl;
+	double TotB=BP+Bpas;
+	cout<<"Total Biaya  (Rp)	: "<<TotB<<endl;
+	cout<<"-------------------------------------";
+	getch();
+	return 0;
+	
+}
+
+
